83773c5407f6069fa3994c00c16d56993e7191b4
50516004 <iwakura.kenshin.15@shizuoka.ac.jp>
2024-07-10 15:42:31 +0900

    CsvEditorページを作成

--- src/app/home/files/page.tsx
+++ src/app/home/files/page.tsx
@@ -1,12 +1,10 @@
-import { BlobList } from "@/src/ui/blob/blob-list";
-import { Form } from "@/src/ui/blob/text-form";
+import ViewTable from "@/src/ui/csv/view-table";
 
 export default async function Page() {
 
   return (
     <div className="w-full">
-      <Form/>
-      <BlobList />
+      <ViewTable/>
     </div>
   );
 }
